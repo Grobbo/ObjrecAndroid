73e9ede761554ee4c797cae6966ed100ea01d9f8
Grobbo <claes.erik.sundstrom@gmail.com>
2017-05-15 09:58:28 +0200

    State

---
+++ app/src/main/java/com/example/ludvig/opencvtest/State.java
@@ -0,0 +1,21 @@
+package com.example.ludvig.opencvtest;
+
+/**
+ * Created by Erik on 2017-05-15.
+ */
+
+public class State {
+
+    int high_hValue;
+    int high_sValue;
+    int high_vValue;
+    int low_hValue;
+    int low_sValue;
+    int low_vValue;
+    boolean isMenu = true;
+    boolean isRBG = true;
+
+    public State(){
+
+    }
+}
